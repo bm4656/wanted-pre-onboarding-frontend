d2dda7f6e1a40ac87c0ca87b81088d48e118aecd
bm4656 <newjin46@gmail.com>
2023-02-08 01:17:28 +0900

    feat: navbar link 설정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className='flex justify-around p-2 bg-slate-200 text-blue-500 text-lg'>
+      <Link to='/' className='focus:text-purple-500'>
+        Intro
+      </Link>
+      <Link to='/signup' className='focus:text-purple-500'>
+        SignUp
+      </Link>
+      <Link to='/signin' className='focus:text-purple-500'>
+        SignIn
+      </Link>
+      <Link to='/todo' className='focus:text-purple-500'>
+        TodoList
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
