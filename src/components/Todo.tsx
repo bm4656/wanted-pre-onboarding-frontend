250ed206240aec83945083e459d93e0fc223d791
bm4656 <newjin46@gmail.com>
2023-02-10 04:56:20 +0900

    feat: todo 수정하기

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Todo = ({ id, todo, isCompleted, onUpdate, onDelete }: any) => {
-  const handleUpdate = (e: any) => {
+  const [modify, setModify] = useState(false);
+  const [text, setText] = useState('');
+  const handleChecked = (e: any) => {
     const isCompleted = e.target.checked;
     onUpdate(id, todo, isCompleted);
   };
   const handleDelete = (e: any) => {
     onDelete(id);
+  };
+  const onModify = () => {
+    setModify(true);
+  };
+  const handleModify = (e: any) => {
+    setText(e.target.value);
+  };
+  const handleUpdate = () => {
+    todo = text;
+    onUpdate(id, todo, isCompleted);
+    setModify(false);
+  };
+  const handleCancel = () => {
+    setModify(false);
   };
   return (
     <li>
@@ -14,14 +30,38 @@ const Todo = ({ id, todo, isCompleted, onUpdate, onDelete }: any) => {
         id={id}
         type='checkbox'
         checked={isCompleted}
-        onChange={handleUpdate}
+        onChange={handleChecked}
       />
-      <label htmlFor={id}>
-        <span>{todo}</span>
-      </label>
-      <button data-testid='modify-button' className='rounded bg-slate-300'>
-        수정
-      </button>
+      {modify ? (
+        <>
+          <input
+            data-testid='modify-input'
+            defaultValue={todo}
+            type='text'
+            onChange={handleModify}
+          />
+          <button data-testid='submit-button' onClick={handleUpdate}>
+            제출
+          </button>
+          <button data-testid='cancel-button' onClick={handleCancel}>
+            취소
+          </button>
+        </>
+      ) : (
+        <>
+          <label htmlFor={id}>
+            <span>{todo}</span>
+          </label>
+          <button
+            data-testid='modify-button'
+            className='rounded bg-slate-300'
+            onClick={onModify}
+          >
+            수정
+          </button>
+        </>
+      )}
+
       <button
         data-testid='delete-button'
         className='rounded bg-red-300'
