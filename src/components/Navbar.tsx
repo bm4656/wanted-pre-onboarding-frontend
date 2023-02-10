80802dd04edf2283321c77f7c2d949d9f41ecc6e
bm4656 <newjin46@gmail.com>
2023-02-11 02:09:50 +0900

    feat: 임시 로그아웃 기능

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <nav className='flex justify-around p-2 bg-slate-200 text-blue-500 text-lg'>
-      <Link to='/' className='focus:text-purple-500'>
-        Intro
-      </Link>
-      <Link to='/signup' className='focus:text-purple-500'>
-        SignUp
-      </Link>
-      <Link to='/signin' className='focus:text-purple-500'>
-        SignIn
-      </Link>
-      <Link to='/todo' className='focus:text-purple-500'>
-        TodoList
-      </Link>
-    </nav>
-  );
+  const isLogin = localStorage.getItem('accessToken');
+  const navigate = useNavigate();
+  if (isLogin)
+    return (
+      <nav className='flex justify-around p-2 bg-slate-200 text-blue-500 text-lg'>
+        <Link to='/' className='focus:text-purple-500'>
+          Intro
+        </Link>
+        <Link to='/todo' className='focus:text-purple-500'>
+          TodoList
+        </Link>
+        <button
+          onClick={() => {
+            localStorage.removeItem('accessToken');
+            alert('로그아웃 되었습니다.');
+            navigate('/');
+          }}
+        >
+          Logout
+        </button>
+      </nav>
+    );
+  else
+    return (
+      <nav className='flex justify-around p-2 bg-slate-200 text-blue-500 text-lg'>
+        <Link to='/' className='focus:text-purple-500'>
+          Intro
+        </Link>
+        <Link to='/signup' className='focus:text-purple-500'>
+          SignUp
+        </Link>
+        <Link to='/signin' className='focus:text-purple-500'>
+          SignIn
+        </Link>
+      </nav>
+    );
 };
 
 export default Navbar;
